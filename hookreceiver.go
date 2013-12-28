d2e5fd094c8afe4d13782f5c814525b5a01b3900
Stefan Reuter <stefan.reuter@reucon.com>
2013-12-28 03:14:14 +0100

    Handle notifications asynchronously

--- hookreceiver.go
+++ hookreceiver.go
@@ -16,24 +16,14 @@ type HookReceiveServer struct {
 	Parse Parse
 }
 
-func (s HookReceiveServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	notification, err := s.Parse(r)
-	if err != nil {
-		log.Printf("Unable to parse request: %s\n", err)
-		return
-	}
-
-	repo := notification.RepositoryUrl()
-	branches := notification.Branches()
-
-	fmt.Fprintf(w, "Ok, thanks. Received notification for repository %q branches %v\n", repo, branches)
-	log.Printf("Received notification for repository %q branches %v", repo, branches)
-
+func handleNotification(notification Notification) {
 	if repositoryConfig, found := config.FindRepositoryConfig(notification); found {
 		log.Printf("Executing command %q in %q", repositoryConfig.Command, repositoryConfig.Dir)
+
 		cmd := exec.Command("/bin/sh", "-c", repositoryConfig.Command)
 		cmd.Dir = repositoryConfig.Dir
 		out, err := cmd.CombinedOutput()
+
 		log.Printf("Command output: %q", string(out))
 		if err != nil {
 			log.Printf("Command exited with error: %s\n", err)
@@ -41,7 +31,24 @@ func (s HookReceiveServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	} else {
 		log.Printf("Repo/branch not configured.")
 	}
+}
 
+func (s HookReceiveServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	notification, err := s.Parse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		fmt.Fprintf(w, "Unable to parse request: %s\n", err)
+		log.Printf("Unable to parse request: %s\n", err)
+		return
+	}
+
+	repo := notification.RepositoryUrl()
+	branches := notification.Branches()
+
+	fmt.Fprintf(w, "Ok, thanks for the notification about repository %q branches %v\n", repo, branches)
+	log.Printf("Received notification for repository %q branches %v", repo, branches)
+
+	go handleNotification(notification)
 }
 
 var configPath string
