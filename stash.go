5867dbb4ff9dd653c4673bccde2e239967ad52dc
Stefan Reuter <stefan.reuter@reucon.com>
2014-04-23 12:42:20 +0200

    Add support for stash and repository names

---
+++ stash.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+// See https://confluence.atlassian.com/display/STASH/POST+service+webhook+for+Stash
+type StashNotification struct {
+	Repository StashRepository
+	RefChanges []StashRefChange
+	Changesets interface{}
+}
+
+type StashRepository struct {
+	Id            int
+	Slug          string
+	Name          string
+	ScmId         string
+	State         string
+	StatusMessage string
+	Forkable      bool
+	Project       StashProject
+	Public        bool
+}
+
+type StashProject struct {
+	Id         int
+	Key        string
+	Name       string
+	Public     bool
+	Type       string
+	IsPersonal bool
+	Owner      StashUser
+}
+
+type StashRefChange struct {
+	RefId    string
+	FromHash string
+	ToHash   string
+	Type     string
+}
+
+type StashUser struct {
+	Name         string
+	EmailAddress string
+	Id           int
+	DisplayName  string
+	Active       bool
+	Slug         string
+	Type         string
+}
+
+func StashParse(r *http.Request) (n Notification, err error) {
+	defer r.Body.Close()
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	return stashParseBytes(bytes)
+}
+
+func stashParseBytes(bytes []byte) (n StashNotification, err error) {
+	err = json.Unmarshal(bytes, &n)
+	return
+}
+
+func (n StashNotification) RepositoryUrl() (repositoryUrl string) {
+	repositoryUrl = n.Repository.Project.Key + n.Repository.Name
+	if repositoryUrl[len(repositoryUrl)-1] == '/' {
+		repositoryUrl = repositoryUrl[:len(repositoryUrl)-1]
+	}
+	return
+}
+
+// not supported
+func (n StashNotification) Branches() (branches map[string]bool) {
+	branches = make(map[string]bool)
+	return
+}
---
+++ stash_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type stashParseBytesTest struct {
+	input    string
+	expected StashNotification
+}
+
+var stashParseBytesTests = []stashParseBytesTest{
+	{`{"repository":{"slug":"test","id":61,"name":"test","scmId":"git","state":"AVAILABLE","statusMessage":"Available","forkable":true,"project":{"key":"~SRT","id":2,"name":"Stefan Reuter","type":"PERSONAL","isPersonal":true,"owner":{"name":"srt","emailAddress":"stefan.reuter@example.com","id":5,"displayName":"Stefan Reuter","active":true,"slug":"srt","type":"NORMAL"}},"public":false},"refChanges":[{"refId":"refs/heads/master","fromHash":"8ba5559a89a4c4ae0402cac4f8deec8b8bcb2ae1","toHash":"fec32d23bb614eabd451d8c6e6f9dc14378dcf7c","type":"UPDATE"}],"changesets":{"size":1,"limit":100,"isLastPage":true,"values":[{"fromCommit":{"id":"8ba5559a89a4c4ae0402cac4f8deec8b8bcb2ae1","displayId":"8ba5559"},"toCommit":{"id":"fec32d23bb614eabd451d8c6e6f9dc14378dcf7c","displayId":"fec32d2","author":{"name":"Stefan Reuter","emailAddress":"stefan.reuter@example.com"},"authorTimestamp":1398238608000,"message":"...","parents":[{"id":"8ba5559a89a4c4ae0402cac4f8deec8b8bcb2ae1","displayId":"8ba5559"}]},"changes":{"size":1,"limit":100,"isLastPage":true,"values":[{"contentId":"03ef7017032edcf73758eae1986fb2b772d0f26d","path":{"components":["README.md"],"parent":"","name":"README.md","extension":"md","toString":"README.md"},"executable":false,"percentUnchanged":-1,"type":"MODIFY","nodeType":"FILE","srcExecutable":false,"link":{"url":"/users/srt/repos/test/commits/fec32d23bb614eabd451d8c6e6f9dc14378dcf7c#README.md","rel":"self"},"links":{"self":[{"href":"https://git.example.com/users/srt/repos/test/commits/fec32d23bb614eabd451d8c6e6f9dc14378dcf7c#README.md"}]}}],"start":0,"filter":null},"link":{"url":"/users/srt/repos/test/commits/fec32d23bb614eabd451d8c6e6f9dc14378dcf7c#README.md","rel":"self"},"links":{"self":[{"href":"https://git.example.com/users/srt/repos/test/commits/fec32d23bb614eabd451d8c6e6f9dc14378dcf7c#README.md"}]}}],"start":0,"filter":null}}`,
+		StashNotification{
+			Repository: StashRepository{
+				Slug:          "test",
+				Id:            61,
+				Name:          "test",
+				ScmId:         "git",
+				State:         "AVAILABLE",
+				StatusMessage: "Available",
+				Forkable:      true,
+				Project: StashProject{
+					Key:        "~SRT",
+					Id:         2,
+					Name:       "Stefan Reuter",
+					Type:       "PERSONAL",
+					IsPersonal: true,
+					Owner: StashUser{
+						Name:         "srt",
+						EmailAddress: "stefan.reuter@example.com",
+						Id:           5,
+						DisplayName:  "Stefan Reuter",
+						Active:       true,
+						Slug:         "srt",
+						Type:         "NORMAL",
+					},
+				},
+				Public: false,
+			},
+			RefChanges: []StashRefChange{
+				StashRefChange{
+					RefId:    "refs/heads/master",
+					FromHash: "8ba5559a89a4c4ae0402cac4f8deec8b8bcb2ae1",
+					ToHash:   "fec32d23bb614eabd451d8c6e6f9dc14378dcf7c",
+					Type:     "UPDATE",
+				},
+			},
+		}},
+}
+
+func TestStashParseBytes(t *testing.T) {
+	for _, test := range stashParseBytesTests {
+		actual, err := stashParseBytes([]byte(test.input))
+		if err != nil {
+			t.Error(err)
+		}
+		// Only check Repository and RefChanges as Changesets is unparsed
+		if !reflect.DeepEqual(actual.Repository, test.expected.Repository) {
+			t.Errorf("got %#v, want %#v", actual.Repository, test.expected.Repository)
+		}
+		if !reflect.DeepEqual(actual.RefChanges, test.expected.RefChanges) {
+			t.Errorf("got %#v, want %#v", actual.RefChanges, test.expected.RefChanges)
+		}
+	}
+}
